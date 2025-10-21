345e6578ed24e96734fcd0431752ebd3ce536506
poww <poww04@gmail.com>
2025-10-22 01:33:51 +0800

    auth and reg models

---
+++ app/Models/Registration.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\Hash;
+
+class Registration extends Model
+{
+    use HasFactory;
+
+    protected $table = 'users'; 
+    protected $fillable = [
+        'name',
+        'email',
+        'password',
+        'role',
+    ];
+
+    public function setPasswordAttribute($value)
+    {
+        $this->attributes['password'] = Hash::make($value);
+    }
+}
